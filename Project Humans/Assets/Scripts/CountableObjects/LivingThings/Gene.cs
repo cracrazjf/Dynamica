212b769fa6daaf0a8eb410cc28fe9c0c1caaee09
cracrazjf <47226502+cracrazjf@users.noreply.github.com>
2020-09-28 14:32:54 -0500

    Added Gene and Genome draft scripts, and Changed the Createhuman function in World script

--- Project Humans/Assets/Scripts/CountableObjects/LivingThings/Gene.cs
+++ Project Humans/Assets/Scripts/CountableObjects/LivingThings/Gene.cs
@@ -5,14 +5,67 @@ using UnityEngine;
 
 public class Gene : MonoBehaviour
 {
-        void start() 
+    public string label;
+    public string geneType;
+    public int geneSize;
+    public BitArray geneSequence;
+    public bool mutable;
+    public float specifiedMean;
+    public float specifiedStdev;
+
+    public float mutationRate = 0.01f;
+
+    public Gene(string label, string geneType, int geneSize, BitArray geneSequence, bool mutable, float specifiedMean, float specifiedStdev)
+    {
+        // constructors for all necessary functionale
+        this.label = label;
+        this.geneType = geneType;
+        this.geneSize = geneSize;
+        this.geneSequence = geneSequence;
+        this.mutable = mutable;
+        this.specifiedMean = specifiedMean;
+        this.specifiedStdev = specifiedStdev;
+
+        generateSequence();
+        // create a string called this.output_string which is a string concatenation of all the above variables
+    }
+
+    void start()
     {
 
 
     }
-        void update()
+
+    void update()
     {
 
+
+    }
+
+    void sexualReproduction(Gene fatherGene)
+    {
+        // for each element in the gene sequence
+        for (int i = 0; i < geneSize; i++)
+        {
+
+            // randomly either keep the mother value, or switch to the father value
+            if (UnityEngine.Random.Range(0, 2) == 0)
+            {
+                geneSequence[i] = fatherGene.geneSequence[i];
+            }
+
+            // draw a random float, if < mutation rate, flip the element to its opposite
+            UnityEngine.Random random = new UnityEngine.Random();
+            if (random < mutationRate)
+            {
+                geneSequence[i] = !geneSequence[i];
+            }
+
+        }
+    }
+
+    void generateSequence()
+    {
 
     }
 }
--- Project Humans/Assets/Scripts/CountableObjects/LivingThings/Genome.cs
+++ Project Humans/Assets/Scripts/CountableObjects/LivingThings/Genome.cs
@@ -5,15 +5,71 @@ using UnityEngine;
 
 public class Genome : MonoBehaviour
 {
-    void start() 
+    int numGenes;
+    public List<string> geneLabelList;
+    public Dictionary<string, int> geneDict;
+    public Dictionary<string, string> geneIndexDict;
+
+    // genome.hunger_threshold=''
+    // genome.thirst_threshold=''
+    // genome.sleepiness_threshold=''
+    // genome.fatigue_threshold=''
+    // genome.health_threshold=''
+
+    void start()
     {
 
 
     }
 
-        void update()
+    void update()
     {
 
 
     }
+
+    bool inheretGenome(Genome motherGenome, Genome fatherGenome)
+    {
+        bool success = true;
+
+        if (motherGenome.numGenes == fatherGenome.numGenes)
+        {
+            numGenes = motherGenome.numGenes;
+
+            for (int i = 0; i < numGenes; i++)
+            {
+                if (motherGenome.geneLabelList[i] == fatherGenome.geneLabelList[i])
+                {
+                    label = motherGenome.geneLabelList[i];
+                    geneLabelList.Add(label);
+                    geneIndexDict[label] = i;
+                    childGene = motherGenome.geneDict[label].MemberwiseClone();
+                    childGene.sexualReproduction(fatherGenome.geneDict[label]);
+                    geneDict[label] = childGene;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            return false;
+        }
+        return success;
+    }
+
+    void createGenome()
+    {
+
+        // eventually this needs to be put into a specifies specific config file
+        // and we need a function that is passed the species name so it knows which file to read
+        // and then it reads it in and parses it into this dict
+        Dictionary<string, List<String>> genomeInfoDict = new Dictionary<string, List<String>>();
+
+        public List<string> list1 = new List<string>(new string[] { "int", "5", "mutable", "3", "1" });
+    genomeInfoDict.add("max_velocity", list1);
+    }
+
 }
