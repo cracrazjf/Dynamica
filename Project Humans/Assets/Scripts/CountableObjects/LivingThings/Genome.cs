bff494ed8e9045e80214d8e6ac4e1cafd7cba470
juliajconti <conti3@illinois.edu>
2020-09-30 19:16:25 -0500

    Add config files and allow for read-in

--- Project Humans/Assets/Scripts/CountableObjects/LivingThings/Genome.cs
+++ Project Humans/Assets/Scripts/CountableObjects/LivingThings/Genome.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
 using UnityEngine;
 
 
@@ -21,17 +22,9 @@ public class Genome : MonoBehaviour
     public Dictionary<string, string> constantDict;
     public Dictionary<string, int> constantIndexDict;
 
-    void start()
-    {
+    void start(){}
 
-
-    }
-
-    void update()
-    {
-
-
-    }
+    void update(){}
 
     public bool inheretGenome(Genome motherGenome, Genome fatherGenome)
     {
@@ -66,75 +59,40 @@ public class Genome : MonoBehaviour
         return success;
     }
 
-    
-    public List<string> g1 = new List<string> { "float", "6", "mutable", ".3", ".25", "1"};
-    public List<string> g2 = new List<string> { "float", "6", "mutable", ".4", ".25", "1"};
-    public List<string> g3 = new List<string> { "float", "6", "mutable", ".5", ".25", "1"};
-    public List<string> g4 = new List<string> { "float", "6", "mutable", ".6", ".25", "1"};
-    public List<string> g5 = new List<string> { "float", "6", "mutable", ".7", ".25", "1"};
-    public List<string> g6 = new List<string> { "float", "100", "mutable", ".01", ".0", "1"};
-    public List<string> g7 = new List<string> { "float", "100", "mutable", ".01", ".0", "1"};
-    public List<string> g8 = new List<string> { "float", "100", "mutable", ".01", ".0", "1"};
-    public List<string> g9 = new List<string> { "float", "100", "mutable", ".01", ".0", "1"};
-    public List<string> g10 = new List<string> { "float", "100", "mutable", ".01", ".0", "1"};
-    public List<string> g11 = new List<string> { "binary", "1", "mutable", "-1", "-1", "1"};
-
-    public List<string> c1 = new List<string> { "0.0", "1"};
-    public List<string> c2 = new List<string> { "50.0", "1"};
-    public List<string> c3 = new List<string> { "0.0", "1"};
-    public List<string> c4 = new List<string> { "0.01", "1"};
-
-    public void importSpeciesInfo(string species){
+    public static void importSpeciesInfo(string species){
         // eventually replace this code with the code that reads from the file
         // note the config file now has constants and genome examples in it
         // each needs to get read into its own identical data structure
         // the genome items need to get inserted into genomeInfoDict
         // the constants need to get inserted into the constantDict
-        genomeInfoDict.Add("hunger_threshold", g1);
-        genomeInfoDict.Add("thirst_threshold", g2);
-        genomeInfoDict.Add("fatigue_threshold", g3);
-        genomeInfoDict.Add("sleepiness_threshold", g4);
-        genomeInfoDict.Add("health_threshold", g5);
-        genomeInfoDict.Add("hunger_change", g6);
-        genomeInfoDict.Add("thirst_change", g7);
-        genomeInfoDict.Add("fatigue_change", g8);
-        genomeInfoDict.Add("sleepiness_change", g9);
-        genomeInfoDict.Add("health_change", g10);
-        genomeInfoDict.Add("sex", g11);
 
-        constantDict.Add("initial_velocity", c1)
-        constantDict.Add("initial_velocity", c2)
-        constantDict.Add("initial_velocity", c3)
-        constantDict.Add("initial_velocity", c4)
+        // see https://docs.microsoft.com/en-us/dotnet/api/system.configuration.configurationmanager?redirectedfrom=MSDN&view=dotnet-plat-ext-3.1
 
-
-        // then, using the data in genomeInfoDict and constantInfoDict
-        //      create the data structures below algorithmically rather than hard coded
-        numGenes = 11;
-        geneLabelList.Add("hunger_threshold");
-        geneLabelList.Add("thirst_threshold");
-        geneLabelList.Add("fatigue_threshold");
-        geneLabelList.Add("sleepiness_threshold");
-        geneLabelList.Add("health_threshold");
-        geneLabelList.Add("hunger_change");
-        geneLabelList.Add("thirst_change");
-        geneLabelList.Add("fatigue_change");
-        geneLabelList.Add("sleepiness_change");
-        geneLabelList.Add("health_change");
-        geneLabelList.Add("sex");
-        for (int i = 0; i < numGenes; i++){
-            geneIndexDict.Add(geneLabelList[i], i.ToString());
-        }
-
-        numConstants = 4;
-        constantLabelList.add(""); // the label of the constant in a list
-        constantLabelList.add("final_velocity");
-        constantLabelList.add("current_velocity");
-        constantLabelList.add("acceleration_rate");
-        for (int i = 0; i < numConstants; i++){
-            constantIndexDict.Add(constantLabelList[i], i.ToString());
-        }
-
+        var genomeInfo = ConfigurationManager.GenomeInfo;
+        if (genomeInfo.Count == 0)  {  
+            Console.WriteLine("GenomeInfo is empty.");  
+        } else {  
+            int i = 0;
+            foreach (var key in genomeInfo.AllKeys)  {  
+                Console.WriteLine("Key: {0} Value: {1}", key, genomeInfo[key]); 
+                genomeInfoDict.Add(key, genomeInfo[key]);
+                geneLabelList.Add(key);
+                geneIndexDict.Add(key,i);
+                i++;
+            }  
+        } 
+        var genomeConstInfo = ConfigurationManager.GenomeConstInfo;
+        if (genomeContiInfo.Count == 0)  {  
+            Console.WriteLine("GenomeInfo is empty.");  
+        } else {  
+            int j = 0;
+            foreach (var key in genomeConstInfo.AllKeys)  {  
+                Console.WriteLine("Key: {0} Value: {1}", key, genomeInfo[key]);
+                constantLabelList.Add(key);
+                constantIndexDict.Add(key,i);
+                j++;
+            }  
+        } 
     }
 
     public void createGenome(string species)
@@ -144,7 +102,8 @@ public class Genome : MonoBehaviour
         for (int i = 0; i < numGenes; i++){
             Gene newGene = new Gene();
             string label = geneLabelList[i];
-            List<string> geneInfo = new List<string>(genomeInfoDict[label]);
+            string[] geneInfo = text.Split(genomeInfoDict[label]);
+
             newGene.generateGene(label, geneInfo[0], geneInfo[1], geneInfo[2], geneInfo[3], geneInfo[4], geneInfo[5]);
             geneLabelList.Add(label);
             geneDict.Add(label, newGene);
