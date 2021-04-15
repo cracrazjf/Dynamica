a4dff963bcc9da10363e01c15ae6f9f3f5e3e7ca
juliajconti <conti3@illinois.edu>
2021-04-14 21:07:34 -0500

    Fix brackets and detaching torso, NE

--- ProjectHumans/Assets/Scripts/Entities/Animals/Primate/PrimateMotorSystem.cs
+++ ProjectHumans/Assets/Scripts/Entities/Animals/Primate/PrimateMotorSystem.cs
@@ -11,237 +11,198 @@ public class PrimateMotorSystem : MotorSystem
     Vector3 goalPos;
     int footUpdates = 0;
     bool rightStep = true; // change this at some point like handedness
-    ConfigurableJoint leg;
     private float xMin = -1.0f, xMax = 1.0f;
     private float timeValue = 0.0f;
-    public PrimateMotorSystem(Animal animal) : base(animal)
-    {
+
+    public PrimateMotorSystem(Animal animal) : base(animal) {
         Debug.Log("An ape was born!");
         abdomenTrans = thisBody.abdomen.transform;
         primateBody = (PrimateBody)thisBody;
-        leg = thisAnimal.GetGameObject().transform.GetChild(0).GetChild(2).Find("Femur_R").GetComponent<ConfigurableJoint>();
-        Debug.Log(leg);
     }
-    public override void Crouch()
-    {
-        if (stateDict["crouching"] == -1f)
-        {
-            CrouchDown();
-        }
-        else { }
+    
+    public override void Crouch() {
+        if (stateDict["crouching"] == -1f) {
+            CrouchDown(); }
+        else if (stateDict["crouching"] == 1f) {
+            SitUp();
+        } else { Rest(); }
     }
-    public override void Sit()
-    {
-        if (stateDict["sitting"] == -1f)
-        {
+
+    public override void Sit() {
+        if (stateDict["sitting"] == -1f) {
             SitDown();
-        }
-        else { SitUp(); }
+        } else { SitUp(); }
     }
-    public override void Lay()
-    {
-        if (stateDict["laying down"] == -1f)
-        {
+
+    public override void Lay() {
+        if (stateDict["laying down"] == -1f) {
             LayDown();
-        }
-        else { }
+        } else if (stateDict["laying down"] == 1f) {
+            SitUp();
+        } else { Rest(); }
     }
-    public override void Stand()
-    {
-        if (stateDict["standing up"] == 1f)
-        {
+
+    public override void Stand() {
+        if (stateDict["standing up"] == 1f) {
             StandUp();
-        }
-        else { }
+        } else if (stateDict["standing up"] == -1f) {
+            SitDown();
+        } else { Rest(); }
     }
-    public override void Rotate()
-    {
+    public override void Rotate() {
         float degree = stateDict["rotating"] * 0.5f;
         float rotatingSpeed = degree * thisAnimal.GetPhenotype().GetTrait("max_rotation");
+
         thisBody.globalPos.Rotate(0, rotatingSpeed, 0, Space.World);
     }
-    public override void TakeSteps()
-    {
-        //Debug.Log("Walking");
+    public override void TakeSteps() {
         float degree = stateDict["taking steps"];
         float stepProportion = degree * thisAnimal.GetPhenotype().GetTrait("max_step") * 0.5f;
         float xQ = Mathf.Lerp(xMin, xMax, timeValue);
         timeValue += 0.5f * Time.deltaTime;
-        if (timeValue > 1.0f)
-        {
+
+        if (timeValue > 1.0f) {
             float temp = xMax;
             xMax = xMin;
             xMin = temp;
             timeValue = 0.0f;
         }
+
+        ConfigurableJoint leg = thisBody.GetSkeleton("Femur_R").GetComponent<ConfigurableJoint>();
         leg.targetRotation = new Quaternion(xQ, 0, 0, 1);
-        if (footUpdates < 500)
-        {
+
+        if (footUpdates < 500) {
             LegUp(rightStep);
             footUpdates++;
-        }
-        else
-        {
+        } else {
             footUpdates = 0;
             rightStep = !rightStep;
         }
-        abdomenTrans.Translate(thisBody.GetGameObject().transform.forward * stepProportion * Time.deltaTime);
+        
+        // Super important to move parts of the body, not the whole gameObject. 
+        abdomenTrans.Translate(abdomenTrans.forward * stepProportion * Time.deltaTime);
     }
-    public override void UseHand()
-    {
+    public override void UseHand() {
         GameObject holder = GetActiveHand();
-        if (stateDict["hand action"] == 1f)
-        {
+
+        if (stateDict["hand action"] == 1f) { 
             PickUp();
-        }
-        else
-        {
-            DropArm();
-        }
+        } else { DropArm(); }
     }
-    public override void Consume()
-    {
+    public override void Consume() {
         Debug.Log("Tried to eat something");
         GameObject holder = GetActiveHand();
-        if (ArmTo(thisBody.head.transform.position))
-        {
+
+        if (ArmTo(thisBody.head.transform.position)) {
             thisBody.EatObject(holder.name);
             DropArm();
         }
     }
-    public override void Sleep()
-    {
-        if (stateDict["consuming"] == 1f)
-        {
+    public override void Sleep() {
+        if (stateDict["consuming"] == 1f) {
             FallAsleep();
-        }
-        else { WakeUp(); }
+        } else { WakeUp(); }
     }
-    public override void Rest()
-    {
+
+    public override void Rest() {
         Debug.Log("Resting");
         thisBody.RestAdjust();
     }
-    public override void Look()
-    {
-        if (stateDict["looking"] == 1f)
-        {
+
+    public override void Look() {
+        if (stateDict["looking"] == 1f) {
             LookAt();
-        }
-        else { LookForward(); }
+        } else { LookForward(); }
     }
+
     // BEGIN HELPER FUNCTIONS
-    void SitDown()
-    {
-        if (thisBody.CheckCrouching())
-        {
+    void SitDown() {
+        if (thisBody.CheckCrouching()) {
             Vector3 toSend = abdomenTrans.position;
             double sitHeight = thisBody.GetHeight() / 4.0;
-            if (toSend.y > sitHeight)
-            {
+            if (toSend.y > sitHeight) {
                 thisBody.DisableKinematic("Abdomen");
-            }
-            else
-            {
+            } else {
                 thisBody.EnsureKinematic("Abdomen");
                 Debug.Log("I think I'm sitting");
             }
-        }
-        else
-        {
-            CrouchDown();
-        }
+        } else { CrouchDown(); }
     }
-    void SitUp()
-    {
-        if (thisBody.GetState("laying") == 1f)
-        {
+    void SitUp() {
+        if (thisBody.GetState("laying") == 1f) {
             BendLegs(45f, 0f);
-        }
-        else
-        {
+        } else {
             thisBody.RotateJointTo("Hips", new Quaternion(0, 0, 0, 0));
             BendLegs(0f, 0f);
             BendKnees(0f);
         }
     }
-    void LayDown()
-    {
+
+    void LayDown() {
         Debug.Log("Tried to lay down");
-        if (thisAnimal.GetBodyState("sitting"))
-        {
+
+        if (thisAnimal.GetBodyState("sitting")) {
             Collapse();
-        }
-        else { SitDown(); }
+        } else { SitDown(); }
     }
-    // Functional
-    void StandUp()
-    {
+    
+    void StandUp() {
         Vector3 toSend = abdomenTrans.position;
-        if (toSend.y < thisBody.GetHeight())
-        {
+
+        if (toSend.y < thisBody.GetHeight()) {
             abdomenTrans.Translate(Vector3.up * (Time.deltaTime));
-        }
-        else { Debug.Log("I think I'm standing"); }
+        } else { Debug.Log("I think I'm standing"); }
     }
-    // Untested
-    void PickUp()
-    {
+    
+    void PickUp() {
         Debug.Log("Tried to pick something up");
         GameObject holder = GetActiveHand();
-        if (ArmToGoal())
-        {
+
+        if (ArmToGoal()) {
             GrabWithHolder(holder);
             DropArm();
         }
     }
-    // Untested
-    void SetDown()
-    {
+    
+    void SetDown() {
         Debug.Log("Tried to set something down");
         GameObject holder = GetActiveHand();
-        if (thisBody.CheckSitting())
-        {
+        if (thisBody.CheckSitting()) {
             RemoveFromHolder(holder);
-        }
-        else { KneelDown(); }
+        } else { KneelDown(); }
     }
     // Functional
-    void WakeUp()
-    {
+    void WakeUp() {
         Debug.Log("Waking up");
         ToggleEyes(true);
         thisBody.SetState("sleeping", -1f);
     }
-    // Nonfunctional - no sleep adjustment
-    void FallAsleep()
-    {
+
+    void FallAsleep() {
         Debug.Log("Sleeping");
         ToggleEyes(false);
-        if (thisBody.CheckLaying())
-        {
+
+        if (thisBody.CheckLaying()) {
             thisBody.SetState("sleeping", 1f);
             thisBody.SleepAdjust();
-        }
-        else { LayDown(); }
+        } else { LayDown(); }
     }
+
     // Functional (but hated)
-    void LookForward()
-    {
+    void LookForward() {
         Quaternion toSend = Quaternion.LookRotation(thisBody.globalPos.forward);
         thisBody.RotateJointTo("Head", toSend);
     }
+
    // Need to look at active hand
-    void LookAt()
-    {
+    void LookAt() {
         GameObject holder = GetActiveHand();
         Vector3 direction = (holder.transform.position - thisBody.head.transform.position).normalized;
         Quaternion toSend = Quaternion.LookRotation(direction);
         thisBody.RotateJointTo("Head", toSend);
     }
-    // Untested
-    bool ArmTo(Vector3 targetPos)
-    {
+    
+    bool ArmTo(Vector3 targetPos) {
         string arm = GetActiveHand().name;
         thisBody.EnsureKinematic(arm);
         Transform armTrans = thisBody.GetSkeletonDict()[arm].transform;
@@ -249,38 +210,32 @@ public class PrimateMotorSystem : MotorSystem
         return (armTrans.position == targetPos);
     }
     // Functional
-    void BendKnees(float degree)
-    {
+    void BendKnees(float degree) {
         Quaternion toSend = new Quaternion(degree, 0, 0, 0);
         thisBody.RotateJointBy("Tibia_L", toSend);
         thisBody.RotateJointBy("Tibia_R", toSend);
     }
-    void LegUp(bool right)
-    {
+    void LegUp(bool right) {
         Quaternion forward = new Quaternion(30, 0, 0, 0);
         Quaternion back = new Quaternion(-30, 0, 0, 0);
-        if (right)
-        {
+
+        if (right) {
             thisBody.RotateJointBy("Tibia_L", back);
             thisBody.RotateJointBy("Tibia_R", forward);
-        }
-        else
-        {
+        } else {
             thisBody.RotateJointBy("Tibia_L", forward);
             thisBody.RotateJointBy("Tibia_R", back);
         }
     }
-    // Functional
-    void BendLegs(float xDegree, float yDegree)
-    {
+    
+    void BendLegs(float xDegree, float yDegree) {
         Quaternion sendLeft = new Quaternion(xDegree, yDegree, 0, 0);
         Quaternion sendRight = new Quaternion(xDegree, -yDegree, 0, 0);
         thisBody.RotateJointBy("Femur_L", sendLeft);
         thisBody.RotateJointBy("Femur_R", sendRight);
     }
-    // Untested
-    bool ArmToGoal()
-    {
+
+    bool ArmToGoal() {
         Vector3 abAdj = primateBody.abAdjLeft;
         string arm = GetActiveHand().name;
         if (stateDict["active right"] == 1f) {
@@ -307,83 +262,68 @@ public class PrimateMotorSystem : MotorSystem
         return (armTrans.position == goalPos);
     }
     // Functional
-    void DropArm()
-    {
+    void DropArm() {
         string arm = GetActiveHand().name;
         thisBody.DisableKinematic(arm);
     }
-    // Functional
-    void KneelDown()
-    {
+    
+    void KneelDown() {
         Vector3 toSend = abdomenTrans.position;
-        if (toSend.y > thisBody.GetHeight() / 2 + 0.5)
-        {
+        if (toSend.y > thisBody.GetHeight() / 2 + 0.5) {
             BendLegs(30f, 0f);
             BendKnees(-45f);
             abdomenTrans.Translate(Vector3.up * (Time.deltaTime * -1));
-        }
-        else { Debug.Log("I think I'm kneeling"); }
+        } else { Debug.Log("I think I'm kneeling"); }
     }
-    // Functional
-    void CrouchDown()
-    {
+
+    void CrouchDown() {
         Debug.Log("Crouch was called");
-        if (thisBody.CheckCrouching())
-        {
+        if (thisBody.CheckCrouching()) {
             BendLegs(-1f, 0f);
             BendLegs(80f, 0f);
             BendKnees(-30f);
             abdomenTrans.Translate(Vector3.up * (Time.deltaTime * -1));
-        }
-        else { thisBody.SetState("crouching", 1f); }
+        } else { thisBody.SetState("crouching", 1f); }
     }
-    // Functional
-    void Collapse()
-    {
+
+    void Collapse() {
         Debug.Log("Collapsing");
         thisBody.DisableKinematic("Abdomen");
     }
-    // Untested
-    void GrabWithHolder(GameObject holder)
-    {
+
+    void GrabWithHolder(GameObject holder) {
         Debug.Log("Tried to affix something");
         Vector3 forCollider = holder.transform.position;
         Collider[] hitColliders = Physics.OverlapSphere(forCollider, .025f);
         Rigidbody toConnect = holder.GetComponent<Rigidbody>();
-        foreach (var hit in hitColliders)
-        {
-            if (!thisBody.GetSkeletonDict().ContainsKey(hit.gameObject.name))
-            {
-                if (CheckMovableObject(hit.gameObject))
-                {
+
+        foreach (var hit in hitColliders) {
+            if (!thisBody.GetSkeletonDict().ContainsKey(hit.gameObject.name)) {
+                if (CheckMovableObject(hit.gameObject)) {
                     FixedJoint newJoint = hit.gameObject.AddComponent<FixedJoint>() as FixedJoint;
                     newJoint.connectedBody = toConnect;
-                }
-                else { Debug.Log("Object too big to lift!"); }
-            }
-            else { Debug.Log("Nothing to pick up!"); }
+                } else { Debug.Log("Object too big to lift!"); }
+            } else { Debug.Log("Nothing to pick up!"); }
         }
     }
-    void RemoveFromHolder(GameObject holder)
-    {
+
+    void RemoveFromHolder(GameObject holder) {
         string holderName = holder.name;
         if (thisBody.GetHeld(holderName) != null) { thisBody.RemoveObject(holderName); }
     }
-    bool CheckMovableObject(GameObject toLift)
-    {
+
+    bool CheckMovableObject(GameObject toLift) {
         float liftMass = toLift.GetComponent<Rigidbody>().mass;
         return (liftMass < thisBody.rigidbody.mass);
     }
-    GameObject GetActiveHand()
-    {
-        if (stateDict["active right"] == 1f)
-        {
+
+    GameObject GetActiveHand() {
+        if (stateDict["active right"] == 1f) {
             return thisBody.GetSkeleton("Hand_R");
-        }
-        else { return thisBody.GetSkeleton("Hand_L"); }
+        } else { return thisBody.GetSkeleton("Hand_L"); }
     }
-    void ToggleEyes(bool open)
-    {
+
+    void ToggleEyes(bool open) {
         thisBody.GetSkeleton("Eye_R").SetActive(open);
         thisBody.GetSkeleton("Eye_L").SetActive(open);
     }
