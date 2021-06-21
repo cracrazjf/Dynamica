de375aa042ec9319d5d4ff03b42cdf8cd57b5d00
cracrazjf <47226502+cracrazjf@users.noreply.github.com>
2021-06-21 09:42:34 -0500

    updates

---
+++ ProjectHumans/Assets/Scripts/Test/TestMotor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMotor : MonoBehaviour
+{
+    public Transform abdomenTrans;
+    public Transform bodyTrans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TakingSteps();
+        //Rotate();
+    }
+
+    void TakingSteps()
+    {
+        bodyTrans.Translate(bodyTrans.forward * 2 * Time.deltaTime, Space.World);
+    }
+    void Rotate()
+    {
+        bodyTrans.Rotate(0, 10 * Time.deltaTime, 0, Space.World);
+    }
+    
+}
