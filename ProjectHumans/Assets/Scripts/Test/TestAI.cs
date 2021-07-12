1a969db9fe8bf0c56ffc81656384316bd7f5048a
cracrazjf <47226502+cracrazjf@users.noreply.github.com>
2021-07-11 21:08:45 -0500

    Updates on simpleAI

--- ProjectHumans/Assets/Scripts/Test/TestAI.cs
+++ ProjectHumans/Assets/Scripts/Test/TestAI.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class TestAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<GameObject> inSight = new List<GameObject>();
+    public TestMotor motor;
+    public bool facingTarget;
     void Start()
     {
         
@@ -13,6 +15,135 @@ public class TestAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateFOV(transform, 45, 10);
+        ReachAndGrab();
+    }
+
+    void consume()
+    {
+
+    }
+    void ReachAndGrab()
+    {
+        foreach (GameObject x in inSight)
+        {
+            if (IsReachable(x) || motor.reached)
+            {
+                
+                if (motor.isCrouching)
+                {
+                    if (motor.leftHand.childCount > 1)
+                    {
+                        motor.Stand();
+                        motor.PutDownHand(-1);
+                    }
+                    else
+                    {
+                        motor.UseHand(-1, 1, 0.2f, 0.33f);
+                    }
+                }
+                else
+                {
+                    motor.Crouch();
+                }
+            }
+            else
+            {
+                if (x.CompareTag("Water"))
+                {
+                    FacePosition(x.transform.position);
+                    if (facingTarget)
+                    {
+                        motor.TakingSteps();
+                    }
+                }
+            }
+        }
+    }
+    public void FacePosition(Vector3 targetPos)
+    {
+        if (!IsFacing(targetPos))
+        {
+            if (GetRelativePosition(targetPos) == -1)
+            {
+                motor.Rotate(-10);
+            }
+            else
+            {
+                motor.Rotate(10);
+            }
+        }
+    }
+    public int GetRelativePosition(Vector3 targetPos)
+    {
+        Vector3 relativePosition = transform.InverseTransformPoint(targetPos);
+        if (relativePosition.x < 0)
+        {
+            return -1;
+        }
+        else if (relativePosition.x > 0)
+        {
+            return 1;
+        }
+        return 0;
+    }
+    public bool IsFacing(Vector3 targetPos)
+    {
+        float angle = Vector3.Angle(transform.forward, targetPos - transform.position);
+        if (angle <= 13f)
+        {
+            facingTarget = true;
+            return true;
+        }
+        facingTarget = false;
+        return false;
+    }
+    public bool IsReachable(GameObject target)
+    {
+        float distance = Vector3.Distance(transform.position, target.transform.position);
+        if (distance < 1.0f)
+        {
+            return true;
+        }
+        return false;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, 10);
+
+        Gizmos.color = Color.blue;
+    }
+
+
+    public void UpdateFOV(Transform checkingObject, float maxAngle, float maxRadius)
+    {
+        LayerMask layermask = ~(1 << 9 | 1 << 8);
+        Collider[] overlaps = new Collider[60];
+        int count = Physics.OverlapSphereNonAlloc(checkingObject.position, maxRadius, overlaps, layermask);
+        inSight.Clear();
+        for (int i = 0; i < count + 1; i++)
+        {
+            if (overlaps[i] != null)
+            {
+                Vector3 directionBetween = (overlaps[i].transform.position - checkingObject.position).normalized;
+                directionBetween.y *= 0;
+                float angle = Vector3.Angle(checkingObject.forward, directionBetween);
+                if (angle <= maxAngle)
+                {
+                    
+                    Ray ray = new Ray(checkingObject.position, overlaps[i].transform.position - checkingObject.position);
+                    RaycastHit hit;
+                    if (Physics.Raycast(ray, out hit, maxRadius))
+                    {
+                        
+                        if (hit.transform == overlaps[i].transform)
+                        {
+                            inSight.Add(overlaps[i].gameObject);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- ProjectHumans/Assets/Scripts/Test/TestMotor.cs
+++ ProjectHumans/Assets/Scripts/Test/TestMotor.cs
@@ -6,26 +6,189 @@ public class TestMotor : MonoBehaviour
 {
     public Transform abdomenTrans;
     public Transform bodyTrans;
+    public Transform leftEye;
+    public Transform rightEye;
+    public Transform leftHand;
+    public Transform rightHand;
+    Vector3 goalPos;
+    int footUpdates = 0;
+    bool firstRight = true; // change this at some point like handedness
+    private float xMin = -1f, xMax = 1f;
+    private float timeValue = 0.0f;
+    public HingeJoint rightFemur;
+    public HingeJoint leftFemur;
+    public HingeJoint rightTibia;
+    public HingeJoint leftTibia;
+    public HingeJoint rightHumerus;
+    public HingeJoint leftHumerus;
+    public HingeJoint rightRadius;
+    public HingeJoint leftRadius;
+    public HingeJoint neck;
+    public bool setAxis;
+    public bool isCrouching;
+    public bool reached;
+    public bool consumed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Crouch();
         //TakingSteps();
-        //Rotate();
+        //Rotate(-10);
+        //IsCrouching();
     }
 
-    void TakingSteps()
+    public void TakingSteps()
     {
         bodyTrans.Translate(bodyTrans.forward * 2 * Time.deltaTime, Space.World);
     }
-    void Rotate()
+    public void Rotate(float angle)
     {
-        bodyTrans.Rotate(0, 10 * Time.deltaTime, 0, Space.World);
+        bodyTrans.Rotate(0, angle * Time.deltaTime, 0, Space.World);
     }
-    
+    public void Crouch()
+    {
+        JointSpring femurHingeSpring = rightFemur.spring;
+        JointSpring tibiaHingeSpring = rightTibia.spring;
+        femurHingeSpring.targetPosition = -120;
+        rightFemur.spring = femurHingeSpring;
+        leftFemur.spring = femurHingeSpring;
+        tibiaHingeSpring.targetPosition = 160;
+        rightTibia.spring = tibiaHingeSpring;
+        leftTibia.spring = tibiaHingeSpring;
+        if (abdomenTrans.localPosition.y > -1.3)
+        {
+            abdomenTrans.Translate(-abdomenTrans.up * 2 * Time.deltaTime, Space.Self);
+        }
+        else
+        {
+            if (abdomenTrans.localRotation.x < 0.5)
+            {
+                abdomenTrans.Rotate(30 * Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                isCrouching = true;
+            }
+        }
+
+
+
+    }
+    public void Stand()
+    {
+        if (abdomenTrans.localPosition.y < -0.25)
+        {
+            abdomenTrans.Translate(Vector3.up * 2 * Time.deltaTime, Space.Self);
+        }
+        if (abdomenTrans.localRotation.x > 0)
+        {
+            abdomenTrans.Rotate(-30 * Time.deltaTime, 0, 0);
+        }
+        JointSpring femurHingeSpring = rightFemur.spring;
+        JointSpring tibiaHingeSpring = rightTibia.spring;
+        femurHingeSpring.targetPosition = 0;
+        rightFemur.spring = femurHingeSpring;
+        leftFemur.spring = femurHingeSpring;
+        tibiaHingeSpring.targetPosition = 0;
+        rightTibia.spring = tibiaHingeSpring;
+        leftTibia.spring = tibiaHingeSpring;
+    }
+    public void UseHand(float hand, float reach_x, float reach_z, float angle)
+    {
+        LayerMask layermask = ~(1 << 8 | 1 << 9);
+        if (hand > 0)
+        {
+            if (!setAxis)
+            {
+                Vector3 axis = new Vector3(reach_x, 0, reach_z);
+                rightHumerus.axis = axis;
+                setAxis = true;
+            }
+            JointSpring humerusHingeSpring = rightHumerus.spring;
+            humerusHingeSpring.targetPosition = angle * 180;
+            rightHumerus.spring = humerusHingeSpring;
+            int maxColliders = 10;
+            Collider[] hitColliders = new Collider[maxColliders];
+            int numColliders = Physics.OverlapSphereNonAlloc(rightHand.position, 0.5f, hitColliders, layermask);
+            if (numColliders > 0)
+            {
+                hitColliders[0].transform.parent = rightHand;
+                rightHand.GetChild(0).localPosition = new Vector3(0, 0, 0);
+                reached = true;
+            }
+        }
+        if (hand < 0)
+        {
+            if (!setAxis)
+            {
+                Vector3 axis = new Vector3(reach_x, 0, reach_z);
+                leftHumerus.axis = axis;
+                setAxis = true;
+            }
+            JointSpring humerusHingeSpring = rightHumerus.spring;
+            humerusHingeSpring.targetPosition = angle * 180;
+            leftHumerus.spring = humerusHingeSpring;
+            int maxColliders = 10;
+            Collider[] hitColliders = new Collider[maxColliders];
+            int numColliders = Physics.OverlapSphereNonAlloc(leftHand.position, 0.5f, hitColliders, layermask);
+            if (numColliders > 0)
+            {
+                hitColliders[0].transform.GetComponent<Rigidbody>().isKinematic = true;
+                hitColliders[0].transform.parent = leftHand;
+                leftHand.GetChild(1).localPosition = new Vector3(0, 0, 0);
+                leftHand.GetChild(1).localRotation = Quaternion.Euler(283.640747f, 84.107254f, 138.337387f);
+                reached = true;
+            }
+        }
+    }
+    public void PutDownHand(int hand)
+    {
+        if (hand > 0)
+        {
+            Vector3 axis = new Vector3(0, 0, 0);
+            rightHumerus.axis = axis;
+            setAxis = false;
+            JointSpring humerusHingeSpring = rightHumerus.spring;
+            humerusHingeSpring.targetPosition = 0;
+            rightHumerus.spring = humerusHingeSpring;
+        }
+        if (hand < 0)
+        {
+            Vector3 axis = new Vector3(0, 0, 0);
+            rightHumerus.axis = axis;
+            setAxis = false;
+            JointSpring humerusHingeSpring = leftHumerus.spring;
+            humerusHingeSpring.targetPosition = 0;
+            leftHumerus.spring = humerusHingeSpring;
+        }
+    }
+    public void Consume(int hand)
+    {
+        if(hand > 0)
+        {
+            JointSpring humerusHingeSpring = rightHumerus.spring;
+            JointSpring radiusHingeSpring = rightRadius.spring;
+            humerusHingeSpring.targetPosition = -50;
+            rightHumerus.spring = humerusHingeSpring;
+            radiusHingeSpring.targetPosition = -130;
+            rightRadius.spring = radiusHingeSpring;
+            consumed = true;
+        }
+        else
+        {
+            JointSpring humerusHingeSpring = leftHumerus.spring;
+            JointSpring radiusHingeSpring = leftRadius.spring;
+            humerusHingeSpring.targetPosition = -50;
+            leftHumerus.spring = humerusHingeSpring;
+            radiusHingeSpring.targetPosition = 130;
+            leftRadius.spring = radiusHingeSpring;
+            consumed = true;
+        }
+    }
 }
